0b5be0178f2f0e37cd0d4559a7ee19f84009a75c
Your Name <you@example.com>
2026-05-20 12:00:41 +0300

    rollback

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,12 +1,6 @@
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const rootEnvPath = path.resolve(__dirname, '../../../.env');
-
-dotenv.config({ path: rootEnvPath });
+dotenv.config();
 
 interface Env {
   port: number;
