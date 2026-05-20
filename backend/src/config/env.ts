4b30dd17bca8a45e7e2b20f5073ad7645af28add
Your Name <you@example.com>
2026-05-20 11:59:36 +0300

    fix

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootEnvPath = path.resolve(__dirname, '../../../.env');
+
+dotenv.config({ path: rootEnvPath });
 
 interface Env {
   port: number;
